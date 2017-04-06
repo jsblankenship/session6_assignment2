918a908852ec031d1d1ec799804a3ddf6f57c8a6
Jeff Blankenship <jsblankenship44@gmail.com>
2017-04-06 14:43:51 -0400

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+var x=15, y=9, z=44; 
+	
+	if(x>y && x>z)
+	{
+		console.log("X is the Greatest Number 15")}
+	
+	if(y>x && y>z)
+	{
+		console.log("Y is the Greatest Number 9")}
+	
+	if(z>x && z>y)
+	{
+		console.log("Z is the Greatest Number 44")}	
